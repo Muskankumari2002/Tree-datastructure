e0e0353fd91a05a1536a0e06f086353f4d3835ea
Muskan kumari <86469435+Muskankumari2002@users.noreply.github.com>
2023-08-20 00:44:14 +0530

    Create level order traversal.cpp

---
+++ level order traversal.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{//define the strucuture for binary tree
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int val):data(val), left(nullptr), right(nullptr){}
+};
+
+void levelordertraversal(Node* root)
+{
+    if(root == nullptr)
+    return;
+    
+    queue<Node*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        Node* current = q.front();
+        q.pop();
+        
+        cout<<current->data<<" ";
+        
+        if(current->left)
+        q.push(current->left);
+        
+        if(current->right)
+        q.push(current->right);
+    }
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    
+    cout<<"Level order Traversal";
+    levelordertraversal(root);
+    return 0;
+}
